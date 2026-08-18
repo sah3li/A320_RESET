cedb8ff4ffe73387edf5eeb31d890c2abcebfc9b
sah3li <sah3lionline@gmail.com>
2026-08-18 22:30:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'a320-reset-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
