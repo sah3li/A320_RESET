14e8c0812dc32dd156a03608b480bf5fc225802b
sah3li <sah3lionline@gmail.com>
2026-08-19 00:02:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'a320-reset-v1';
-const ASSETS = [
+const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './app.js',
@@ -7,10 +7,41 @@ const ASSETS = [
     'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const isGoogleSheet = event.request.url.includes('docs.google.com/spreadsheets');
+
+    if (isGoogleSheet) {
+        // Network First strategy for CSV data
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clonedRes = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clonedRes));
+                    return response;
+                })
+                .catch(() => caches.match(event.request)) // Fallback to cache if offline
+        );
+    } else {
+        // Cache First strategy for App Assets
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                return cachedResponse || fetch(event.request);
+            })
+        );
+    }
 });
